8333d96f4a3bba55928ae162408ef7297aac14c6
Dhruv Ranjan Deshmukh <ddeshmuk@usc.edu>
2025-01-30 02:07:25 +0000

    Recursive linked list

--- split.cpp
+++ split.cpp
@@ -11,6 +11,10 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <cstddef>
+#include <iostream>
+using namespace std;
+
 
 /* Add a prototype for a helper function here if you need */
 
@@ -18,6 +22,26 @@ void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+  if (in == NULL)
+  {
+    return;
+  }
+  Node* nextNode = in -> next;
+  in->next = nullptr;
+  // cout << "Value" << in->value << endl;
+  if (in->value % 2 == 0)
+  {
+    cout << "even" << in->value << endl;
+    evens = in;
+    split(nextNode, odds, evens->next);
+  }
+  else{
+    cout << "odd" << in->value << endl;
+    odds = in;
+    split(nextNode, odds->next, evens);
+  }
+  in = NULL;
+  
 }
 
 /* If you needed a helper function, write it here */
--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,45 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <cstddef>
+#include <iostream>
+using namespace std;
+
+void printlist(Node* head){
+  while(head != NULL)
+  {
+    cout << head->value << ">";
+    head = head->next;
+    cout << "NULL" <<endl;
+
+  }
+}
+
+void deletelist(Node* head){
+  while(head != NULL)
+  {
+    Node* temp = head;
+    head = head->next;
+    delete temp;
+
+  }
+}
 
 int main(int argc, char* argv[])
 {
+   Node* head = new Node(1, new Node(2, new Node(3, new Node(4, new Node(5, new Node(6, NULL))))));
+   Node* odds = NULL;
+   Node* evens = NULL;
 
+   split(head,odds,evens);
+  //  printlist(odds);
+  //  printlist(evens);
+  
+   deletelist(odds);
+   deletelist(evens);
+   
+
+  
 }
+
+
